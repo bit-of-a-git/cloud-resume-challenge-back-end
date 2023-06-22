03945ea849b51e3468fe2844016a73023b8154f0
doc335 <92552641+bit-of-a-git@users.noreply.github.com>
2023-06-23 00:15:26 +0100

    edited Cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,15 @@
 const { defineConfig } = require("cypress");
 
-module.exports = {
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://911e4cqcrk.execute-api.eu-west-1.amazonaws.com/dev",
+  },
   env: {
-    apiEndpoint: process.env.apiEndpoint,
+    apiEndpoint: process.env.apiEndpoint
   },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-};
+});
