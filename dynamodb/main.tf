9f7e605a1e2fdbadb77ac4c6622dab7cdd478ced
doc335 <92552641+bit-of-a-git@users.noreply.github.com>
2023-07-09 01:04:48 +0100

    Revert "removing autoscaling"
    This reverts commit 29ba0cd2357158531c121de2781d3cc25300a533.

--- dynamodb/main.tf
+++ dynamodb/main.tf
@@ -26,51 +26,51 @@ resource "aws_dynamodb_table_item" "hit_count" {
   }
 }
 
-# resource "aws_appautoscaling_target" "hit_counter_table_read_target" {
-#   max_capacity       = 10
-#   min_capacity       = 1
-#   resource_id        = "table/${aws_dynamodb_table.hit_counter.name}"
-#   scalable_dimension = "dynamodb:table:ReadCapacityUnits"
-#   service_namespace  = "dynamodb"
-# }
+resource "aws_appautoscaling_target" "hit_counter_table_read_target" {
+  max_capacity       = 10
+  min_capacity       = 1
+  resource_id        = "table/${aws_dynamodb_table.hit_counter.name}"
+  scalable_dimension = "dynamodb:table:ReadCapacityUnits"
+  service_namespace  = "dynamodb"
+}
 
-# resource "aws_appautoscaling_policy" "hit_counter_table_read_policy" {
-#   name               = "dynamodb-read-capacity-utilization-${aws_appautoscaling_target.hit_counter_table_read_target.resource_id}"
-#   policy_type        = "TargetTrackingScaling"
-#   resource_id        = aws_appautoscaling_target.hit_counter_table_read_target.resource_id
-#   scalable_dimension = aws_appautoscaling_target.hit_counter_table_read_target.scalable_dimension
-#   service_namespace  = aws_appautoscaling_target.hit_counter_table_read_target.service_namespace
+resource "aws_appautoscaling_policy" "hit_counter_table_read_policy" {
+  name               = "dynamodb-read-capacity-utilization-${aws_appautoscaling_target.hit_counter_table_read_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.hit_counter_table_read_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.hit_counter_table_read_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.hit_counter_table_read_target.service_namespace
 
-#   target_tracking_scaling_policy_configuration {
-#     predefined_metric_specification {
-#       predefined_metric_type = "DynamoDBReadCapacityUtilization"
-#     }
-#     target_value = 70
-#   }
-# }
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBReadCapacityUtilization"
+    }
+    target_value = 70
+  }
+}
 
-# resource "aws_appautoscaling_target" "hit_counter_table_write_target" {
-#   max_capacity       = 10
-#   min_capacity       = 1
-#   resource_id        = "table/${aws_dynamodb_table.hit_counter.name}"
-#   scalable_dimension = "dynamodb:table:WriteCapacityUnits"
-#   service_namespace  = "dynamodb"
-# }
+resource "aws_appautoscaling_target" "hit_counter_table_write_target" {
+  max_capacity       = 10
+  min_capacity       = 1
+  resource_id        = "table/${aws_dynamodb_table.hit_counter.name}"
+  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
+  service_namespace  = "dynamodb"
+}
 
-# resource "aws_appautoscaling_policy" "hit_counter_table_write_policy" {
-#   name               = "dynamodb-write-capacity-utilization-${aws_appautoscaling_target.hit_counter_table_write_target.resource_id}"
-#   policy_type        = "TargetTrackingScaling"
-#   resource_id        = aws_appautoscaling_target.hit_counter_table_write_target.resource_id
-#   scalable_dimension = aws_appautoscaling_target.hit_counter_table_write_target.scalable_dimension
-#   service_namespace  = aws_appautoscaling_target.hit_counter_table_write_target.service_namespace
+resource "aws_appautoscaling_policy" "hit_counter_table_write_policy" {
+  name               = "dynamodb-write-capacity-utilization-${aws_appautoscaling_target.hit_counter_table_write_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.hit_counter_table_write_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.hit_counter_table_write_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.hit_counter_table_write_target.service_namespace
 
-#   target_tracking_scaling_policy_configuration {
-#     predefined_metric_specification {
-#       predefined_metric_type = "DynamoDBWriteCapacityUtilization"
-#     }
-#     target_value = 70
-#   }
-# }
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
+    }
+    target_value = 70
+  }
+}
 
 resource "aws_dynamodb_table" "ip_address" {
   name           = "ip_addresses${var.append}"
@@ -93,48 +93,48 @@ resource "aws_dynamodb_table" "ip_address" {
   }
 }
 
-# resource "aws_appautoscaling_target" "ip_address_table_read_target" {
-#   max_capacity       = 10
-#   min_capacity       = 1
-#   resource_id        = "table/${aws_dynamodb_table.ip_address.name}"
-#   scalable_dimension = "dynamodb:table:ReadCapacityUnits"
-#   service_namespace  = "dynamodb"
-# }
+resource "aws_appautoscaling_target" "ip_address_table_read_target" {
+  max_capacity       = 10
+  min_capacity       = 1
+  resource_id        = "table/${aws_dynamodb_table.ip_address.name}"
+  scalable_dimension = "dynamodb:table:ReadCapacityUnits"
+  service_namespace  = "dynamodb"
+}
 
-# resource "aws_appautoscaling_policy" "ip_address_table_read_policy" {
-#   name               = "dynamodb-read-capacity-utilization-${aws_appautoscaling_target.ip_address_table_read_target.resource_id}"
-#   policy_type        = "TargetTrackingScaling"
-#   resource_id        = aws_appautoscaling_target.ip_address_table_read_target.resource_id
-#   scalable_dimension = aws_appautoscaling_target.ip_address_table_read_target.scalable_dimension
-#   service_namespace  = aws_appautoscaling_target.ip_address_table_read_target.service_namespace
+resource "aws_appautoscaling_policy" "ip_address_table_read_policy" {
+  name               = "dynamodb-read-capacity-utilization-${aws_appautoscaling_target.ip_address_table_read_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ip_address_table_read_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ip_address_table_read_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ip_address_table_read_target.service_namespace
 
-#   target_tracking_scaling_policy_configuration {
-#     predefined_metric_specification {
-#       predefined_metric_type = "DynamoDBReadCapacityUtilization"
-#     }
-#     target_value = 70
-#   }
-# }
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBReadCapacityUtilization"
+    }
+    target_value = 70
+  }
+}
 
-# resource "aws_appautoscaling_target" "ip_address_table_write_target" {
-#   max_capacity       = 10
-#   min_capacity       = 1
-#   resource_id        = "table/${aws_dynamodb_table.ip_address.name}"
-#   scalable_dimension = "dynamodb:table:WriteCapacityUnits"
-#   service_namespace  = "dynamodb"
-# }
+resource "aws_appautoscaling_target" "ip_address_table_write_target" {
+  max_capacity       = 10
+  min_capacity       = 1
+  resource_id        = "table/${aws_dynamodb_table.ip_address.name}"
+  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
+  service_namespace  = "dynamodb"
+}
 
-# resource "aws_appautoscaling_policy" "ip_address_table_write_policy" {
-#   name               = "dynamodb-write-capacity-utilization-${aws_appautoscaling_target.ip_address_table_write_target.resource_id}"
-#   policy_type        = "TargetTrackingScaling"
-#   resource_id        = aws_appautoscaling_target.ip_address_table_write_target.resource_id
-#   scalable_dimension = aws_appautoscaling_target.ip_address_table_write_target.scalable_dimension
-#   service_namespace  = aws_appautoscaling_target.ip_address_table_write_target.service_namespace
+resource "aws_appautoscaling_policy" "ip_address_table_write_policy" {
+  name               = "dynamodb-write-capacity-utilization-${aws_appautoscaling_target.ip_address_table_write_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ip_address_table_write_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ip_address_table_write_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ip_address_table_write_target.service_namespace
 
-#   target_tracking_scaling_policy_configuration {
-#     predefined_metric_specification {
-#       predefined_metric_type = "DynamoDBWriteCapacityUtilization"
-#     }
-#     target_value = 70
-#   }
-# }
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
+    }
+    target_value = 70
+  }
+}
